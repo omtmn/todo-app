5640fd330ab92a311244d5cda719919a253e7576
Alex <omtmn@icloud.com>
2021-09-13 21:18:26 -0400

    server routes

--- server/api/server.js
+++ server/api/server.js
@@ -3,7 +3,20 @@ const cors = require('cors');
 
 const server = express();
 
+server.use(cors())
 server.use(express.json());
+
+server.get('/api/tasks', (req, res, next) => {
+    res.status(200).json('tasks')
+})
+
+server.post('/api/addTask', (req, res, next) => {
+    res.status(201).json('added task')
+})
+
+server.delete('/api/removeTask', (req, res, next) => {
+    res.status(204).json('Deleted Task')
+})
 
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
