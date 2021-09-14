12a4b9202596f689136b81c88cc04d08c3e7ad26
Alex <omtmn@icloud.com>
2021-09-13 21:45:21 -0400

    wip

--- server/data/migrations/todo-table.js
+++ server/data/migrations/todo-table.js
@@ -4,7 +4,8 @@ exports.up = async function(knex) {
         table.increments('todo_id')
         table.string('todo', 128).notNullable()
         table.string('todo_description', 258)
-        table.integer('todo_complete').defaultTo(0)
+        table.string('todo_notes', 300)
+        table.boolean('todo_complete').defaultTo(false)
         table.timestamps(true, true)
     })
 }
