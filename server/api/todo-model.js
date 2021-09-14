95ada646a2b0fc151623a434776376f2f8815a5d
Alex <omtmn@icloud.com>
2021-09-14 13:24:47 -0400

    minor changes

--- server/api/todo-model.js
+++ server/api/todo-model.js
@@ -4,14 +4,14 @@ function getAll(){
     return db('todos')
 }
 
+async function getById(todo_id) {
+    return db('todos').where('todo_id', todo_id).first() 
+  }
+
 async function addTodo(todo){
     const [todo_id] = await db('todos').insert(todo, "todo_id")
     return getById(todo_id)
 }
-
-async function getById(todo_id) {
-    return db('todos').where('todo_id', todo_id).first() 
-  }
 
 async function deleteTodo(todo_id){
     const deletedTask = await getById(todo_id)
